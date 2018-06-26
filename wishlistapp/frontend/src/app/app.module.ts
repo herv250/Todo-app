3fbd672067a5c2630a3014dac55bc0ebb9d9b66c
Hervé Munyaneza <herve.munyaneza.w3320@student.hogent.be>
2018-06-26 12:28:27 +0200

    added new components

--- wishlistapp/frontend/src/app/app.module.ts
+++ wishlistapp/frontend/src/app/app.module.ts
@@ -3,11 +3,23 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { WishListComponent } from './wishlist/wish-list/wish-list.component';
+import { WishComponent } from './wishlist/wish/wish.component';
+import { LoginComponent } from './user/login/login.component';
+import { LogoutComponent } from './user/logout/logout.component';
+import { RegisterComponent } from './user/register/register.component';
+import { OverviewComponent } from './wishlist/overview/overview.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    WishListComponent,
+    WishComponent,
+    LoginComponent,
+    LogoutComponent,
+    RegisterComponent,
+    OverviewComponent
   ],
   imports: [
     BrowserModule
