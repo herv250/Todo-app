5306edf830974c24497b6c5e60dcc92ab62759a5
Hervé Munyaneza <herve.munyaneza.w3320@student.hogent.be>
2018-08-05 16:23:36 +0200

    added components todo and todo-item

--- todoApp/frontend/src/app/app.module.ts
+++ todoApp/frontend/src/app/app.module.ts
@@ -3,11 +3,17 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { TodoListComponent } from './todo/todo-list/todo-list.component';
+import { TodoComponent } from './todo/todo/todo.component';
+import { TodoOverviewComponent } from './todo/todo-overview/todo-overview.component';
+import { TodoItemComponent } from './todo/todo-item/todo-item.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     TodoListComponent,
+    TodoComponent,
+    TodoOverviewComponent,
+    TodoItemComponent,
   ],
   imports: [
     BrowserModule
