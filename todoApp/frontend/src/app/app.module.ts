4c95f34b6c9796f112d1718f54c1efe1e3889a1b
Hervé Munyaneza <herve.munyaneza.w3320@student.hogent.be>
2018-08-05 15:59:35 +0200

    added todo-list component

--- todoApp/frontend/src/app/app.module.ts
+++ todoApp/frontend/src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { TodoListComponent } from './todo/todo-list/todo-list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    TodoListComponent,
   ],
   imports: [
     BrowserModule
