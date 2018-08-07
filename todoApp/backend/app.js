414a2bff0471c7607223bd76c89da57301f0a4ea
Hervé Munyaneza <herve.munyaneza.w3320@student.hogent.be>
2018-08-07 03:15:58 +0200

    clean backend

--- todoApp/backend/app.js
+++ todoApp/backend/app.js
@@ -4,8 +4,8 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var index = require('./routes/index');
+var users = require('./routes/users');
 
 var app = express();
 
@@ -13,11 +13,12 @@ var app = express();
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+//app.use(bodyParser.json());
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/', index);
+app.use('/users', users);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- todoApp/backend/routes/index.js
+++ todoApp/backend/routes/index.js
@@ -6,4 +6,8 @@ router.get('/', function(req, res, next) {
   res.send('server works');
 });
 
+router.get('/API/recipe/', function(req, res, next) {
+  res.send('process the request here');
+});
+
 module.exports = router;
