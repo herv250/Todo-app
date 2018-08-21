4428d772659dda9f868527d2158302dce6d5ae8f
Hervé Munyaneza <herve.munyaneza.w3320@student.hogent.be>
2018-08-21 22:40:06 +0200

    fixed data service

--- todoApp/frontend/src/app/todo/todo-data.service.ts
+++ todoApp/frontend/src/app/todo/todo-data.service.ts
@@ -4,8 +4,8 @@ import { Todo } from './todo.model';
 import gql from 'graphql-tag';
 import { Apollo } from 'apollo-angular';
 import { Observable } from 'rxjs/Observable';
-import * as Query from './global-query';
-import { ADD_TODO_MUTATION, ALL_TODOLISTS_QUERY } from './global-query';
+import { map } from 'rxjs/operators'
+import { ADD_TODO_MUTATION, ALL_TODOLISTS_QUERY, AllTodolistSResponse, ADD_TODOLIST_MUTATION } from './global-query';
 
 @Injectable({
   providedIn: 'root'
@@ -17,8 +17,8 @@ export class TodoDataService {
   constructor(private apollo: Apollo) {
     console.log('start service');
     this.apollo
-      .watchQuery<Query.AllTodolistSResponse>({
-      query: Query.ALL_TODOLISTS_QUERY
+      .watchQuery<AllTodolistSResponse>({
+      query: ALL_TODOLISTS_QUERY
     })
       .valueChanges.subscribe(({ data }) => {        
         console.log('all boards', data.todolists);
@@ -28,30 +28,51 @@ export class TodoDataService {
       });
   }
 
-  ngOnInit() {
-    this.todoLists;
+  getTodoLists()  {
+    return this.apollo
+      .watchQuery<AllTodolistSResponse>({
+      query: ALL_TODOLISTS_QUERY
+    }).valueChanges
+    .pipe(map(({data}) => {
+      return data.todolists;
+    }));
   }
 
-  get todoLists() {
-    this.apollo
-      .watchQuery<Query.AllTodolistSResponse>({
-      query: Query.ALL_TODOLISTS_QUERY
-    })
-      .valueChanges.subscribe(({ data }) => {
-        console.log('all boards', data.todolists);
-        this._todoLists = data.todolists.map(todoList =>
-          Todolist.fromJSON(todoList)
-        );
-      });
-      return this._todoLists;
-  }
-
-  addTodoList(todoList) {
+  addTodoList(title) {
     console.log('add service');
     //this._todoLists = [...this._todoLists, todoList];
+    if(title){
+      console.log('list title', title);
+      this.apollo.mutate({
+          mutation: ADD_TODOLIST_MUTATION,
+          variables: {
+          title
+          },
+          update: (store, { data: { createTodolist } }) => {
+            //console.log('start update', title);
+            // Read the data from our cache for this query.
+            const data: any = store.readQuery({
+              query: ALL_TODOLISTS_QUERY
+            });
+            // Add our link from the mutation to the end.
+            console.log('before', data.todolists);
+            data.todolists
+              .push(createTodolist);
+              console.log('after', data.todolists);
+            // Write our data back to the cache.
+            store.writeQuery({ query: ALL_TODOLISTS_QUERY, data });
+          }
+      })
+        .subscribe(response => {
+          // We injected the Router service
+          //this.router.navigate(['/']);*/
+          //console.log("should be added", todoTitle);
+        });
+      }
   }
 
   addTodo(todoTitle: string, listId: string) {
+    if(todoTitle && listId){
     console.log('title', todoTitle);
     this.apollo.mutate({
         mutation: ADD_TODO_MUTATION,
@@ -79,7 +100,8 @@ export class TodoDataService {
       .subscribe(response => {
         // We injected the Router service
         //this.router.navigate(['/']);*/
-        console.log("should be added", todoTitle);
+        //console.log("should be added", todoTitle);
       });
+    }
   }
 }
